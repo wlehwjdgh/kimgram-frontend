10f6ed1eed28d801280f1610e291b78b2aee9c7c
wlehwjdgh <wlehwjdgh@gmail.com>
2020-03-10 14:28:43 +0900

    #5.2 Footer and Auth UI part Three

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -6,7 +6,7 @@ import reset from "styled-reset";
 export default createGlobalStyle`
     ${reset};
     @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700');
-    *{
+    * {
         box-sizing:border-box;
     }
     body {
@@ -15,8 +15,11 @@ export default createGlobalStyle`
         font-size:14px;
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
-    a{
-        color:${props=> props.theme.blueColor};
+    a {
+        color:${props => props.theme.blueColor};
         text-decoration:none;
+    }
+    input:focus{
+        outline:none;
     }
 `;
