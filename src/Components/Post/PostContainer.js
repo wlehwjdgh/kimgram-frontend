6c82637ea5b8479d12591047aec2e9fbb6f4d4d7
김정호 <wlehwjdgh@gmail.com>
2020-03-14 22:40:26 +0900

    #7.3 Post Component part Three

--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import useInput from "../../Hooks/useInput";
 import PostPresenter from "./PostPresenter";
@@ -27,7 +27,27 @@ const PostContainer = ({
 
 	*/
 	const [isLikedS, setIsLiked] = useState(isLiked);
-  const [likeCountS, setLikeCount] = useState(likeCount);
+	const [likeCountS, setLikeCount] = useState(likeCount);
+	const [currentItem, setCurrentItem] = useState(0);
+
+
+	const slide= () => {
+		const totalFiles = files.length;
+		if(currentItem === totalFiles -1){
+			setTimeout(() => setCurrentItem(0),3000);
+		}else{
+			setTimeout(() => setCurrentItem(currentItem+1),3000);
+		}
+	}
+
+	/*
+		Post Component가 Mount될때 실행됨
+	*/
+	useEffect(()=>{
+		slide();
+	},[currentItem]);
+	
+	console.log(currentItem);
   const comment = useInput("");
   return (
     <PostPresenter
@@ -41,7 +61,8 @@ const PostContainer = ({
       createdAt={createdAt}
       newComment={comment}
       setIsLiked={setIsLiked}
-      setLikeCount={setLikeCount}
+			setLikeCount={setLikeCount}
+			currentItem={currentItem}
     />
   );
 };
--- src/Components/Post/PostPresenter.js
+++ src/Components/Post/PostPresenter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import TextareaAutosize from "react-autosize-textarea";
 import FatText from "../FatText";
 import Avatar from "../Avatar";
 import { HeartFull, HeartEmpty, Comment } from "../Icons";
@@ -27,12 +28,27 @@ const Location = styled.span`
   font-size: 12px;
 `;
 
-const Files = styled.div``;
-
-const File = styled.img`
-  max-width: 100%;
+const Files = styled.div`
+  position: relative;
+  padding-bottom: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  flex-shrink: 0;
 `;
 
+const File = styled.div`
+  max-width: 100%;
+  width: 100%;
+  height: 600px;
+  position: absolute;
+  top: 0;
+  background-image: url(${props => props.src}});
+  background-size: cover;
+  background-position: center;
+  opacity: ${props => (props.showing ? 1 : 0)};
+  transition: opacity 0.5s linear;
+`;
 const Button = styled.span`
   cursor: pointer;
 `;
@@ -61,13 +77,32 @@ const Timestamp = styled.span`
   border-bottom: ${props => props.theme.lightGreyColor} 1px solid;
 `;
 
+/*
+  아래와 같이 styled component를 만들 수 있다(???)
+  괄호 안에 있는 component가 prob called className을 가지고 있으면 
+  아래처럼 styled component를 추가할 수있다...
+
+  TODO: styled component에 대해 스터디..
+*/
+const Textarea= styled(TextareaAutosize)`
+  border: none;
+  width: 100%;
+  resize: none;
+  font-size:14px;
+  &:focus {
+    outline: none;
+  }
+`;
+
 export default ({
   user: { username, avatar },
   location,
   files,
   isLiked,
   likeCount,
-  createdAt
+  createdAt,
+  newComment,
+  currentItem 
 }) => (
   <Post>
     <Header>
@@ -78,7 +113,8 @@ export default ({
       </UserColumn>
     </Header>
     <Files>
-      {files && files.map(file => <File id={file.id} src={file.url} />)}
+      {files && 
+        files.map( (file, index) => <File key={file.id} src={file.url} showing={index===currentItem}/>)}
     </Files>
     <Meta>
       <Buttons>
@@ -89,6 +125,7 @@ export default ({
       </Buttons>
       <FatText text={likeCount === 1 ? "1 like" : `${likeCount} likes`} />
       <Timestamp>{createdAt}</Timestamp>
+      <Textarea placeholder={"Add a comment..."} {...newComment}/>
     </Meta>
   </Post>
 );
