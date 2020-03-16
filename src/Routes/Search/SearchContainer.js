7265659b5c1b10772a4ba8064d3deee20380c866
wlehwjdgh <wlehwjdgh@gmail.com>
2020-03-16 14:44:37 +0900

    #8.0 Search Screen Queries

---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import SearchPresenter from "./SearchPresenter";
+import { useQuery } from "react-apollo-hooks";
+import { SEARCH } from "./SearchQueries";
+
+export default withRouter(({ location: { search } }) => {
+  const term = search.split("=")[1];
+  
+  /*
+    skip = 만약 파라미터로 요구되는 term이  누락되었다면 쿼리를 실행하지 않는다.
+    skip을 하지 않으면 쿼리가 실행되고 에러가 발생함.
+  */
+  const { data, loading } = useQuery(SEARCH, {
+    skip: term === undefined,
+    variables: {
+      term
+    }
+  });
+  
+  return <SearchPresenter searchTerm={term} loading={loading} data={data} />;
+});
