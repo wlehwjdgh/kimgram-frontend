b8fa638faf9158ce8dde0de05c5131639ba6ceae
wlehwjdgh <wlehwjdgh@gmail.com>
2020-03-16 14:48:37 +0900

    #8.1 Search Screen UI Part One

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -9,7 +9,7 @@ export default withRouter(({ location: { search } }) => {
   
   /*
     skip = 만약 파라미터로 요구되는 term이  누락되었다면 쿼리를 실행하지 않는다.
-    skip을 하지 않으면 쿼리가 실행되고 에러가 발생함.
+    term이 누락된 채로 skip을 하지 않으면 쿼리가 실행되고 에러가 발생함.
   */
   const { data, loading } = useQuery(SEARCH, {
     skip: term === undefined,
